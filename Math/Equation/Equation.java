24048dd6acbc2a28dcf6f2b22f8ed5883ddeb60b
westerhack <westerhack@gmail.com>
2015-12-02 11:20:17 -0800

    Ok, going to start working with equation. Going to not be compiling anything to do with graphing or sets in the mean time, to make less errors happen.

--- Math/Equation/Equation.java
+++ Math/Equation/Equation.java
@@ -11,7 +11,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Equation {
-
     public HashMap<String, CustomFunction> functions;
     public ArrayList<Expression[]> expressions;
 
