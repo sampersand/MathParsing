c03eb3066d271f783a91326f99d766904d09fc5c
westerhack <westerhack@gmail.com>
2016-01-02 01:48:41 -0800

    Everything is working; all that is needed for version 1.0 release is all the code cleaned up.

--- West/Math/Tester.java
+++ West/Math/Tester.java
@@ -37,7 +37,8 @@ public class Tester {
             // eqsys.addConstraint("⊻(>(x, y),<(0, x))");
 
 
-            eqsys.add("=(y, *(x0, *(x1, *(x2, *(x3, x4)))))");
+            eqsys.add("=(y, *(z, ⊻(>(x,0),<(z,0))))");
+            eqsys.add("=(z, *(x0, *(x1, *(x2, *(x3, x4)))))");
             eqsys.add("=(x0, *(x, c)");
             eqsys.add("=(x1, *(-(x, 8), c)");
             eqsys.add("=(x2, *(-(x, 4), c)");
