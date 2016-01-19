6b2b9a1519d85cb3d179e7df6f08bfa4f1b7eb41
westerhack <westerhack@gmail.com>
2016-01-19 09:49:17 -0800

    Partial support for '5cos3x'

--- West/Math/Tester.java
+++ West/Math/Tester.java
@@ -26,7 +26,7 @@ public class Tester {
             // "a=<0-1*sin(2*t)*cos(3*t)-4*cos(5*t)*sin(6*t)-7*cos(8*t)*sin(9*t)-sin(10*t)*cos(11*t),12*sin(13*t)>",
             // "t=5*pi/4"};
             // args = new String[]{"--d","y","--e","v=<4*cos(t)*cos(4*t)-sin(4*t)*sin(t),–2*cos(2*t)>","t=5*pi/4"};
-            args = new String[]{"--d","y","--e","y=5cos(3x)","x=9pi"};
+            args = new String[]{"--d","y","--e","y=5cos3x","x=9pi"};
         }
 
 
